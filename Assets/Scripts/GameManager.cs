1914b5d7963353849b93953c286afacfcef7f90f
GTUNE\ktc <ryoman1002@icloud.com>
2025-06-13 16:43:34 +0900

    メインシーン途中まで

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,16 +6,15 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     private int score = 0;
     public TextMeshProUGUI scoreText;
-<<<<<<< HEAD
+
     public GameObject panel;
     [SerializeField]
-    private Text Text1, Text2, Text3, Text4, Text5, Text6;
-=======
->>>>>>> origin/main
+    
+
 
     void Awake()
     {
-        Instance = this;   
+        Instance = this;
     }
 
     public void AddScore()
@@ -31,8 +30,5 @@ public class GameManager : MonoBehaviour
             scoreText.text = "Score : " + score;
         }
     }
-<<<<<<< HEAD
-=======
-    
->>>>>>> origin/main
+
 }
